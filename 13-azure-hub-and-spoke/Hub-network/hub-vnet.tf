1083e477712bf35d3d004abfccb4d8ff618f110e
learwithpradnya <pradnyadesh10@gmail.com>
2026-02-18 20:27:57 +0530

    Updated

--- 13-azure-hub-and-spoke/Hub-network/hub-vnet.tf
+++ 13-azure-hub-and-spoke/Hub-network/hub-vnet.tf
@@ -1,13 +1,13 @@
 resource "azurerm_virtual_network" "hub_vnet" {
   name                = "hub-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.hub_rg.location
-  resource_group_name = azurerm_resource_group.hub_rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "hub_subnet" {
   name                 = "hub-subnet"
-  resource_group_name  = azurerm_resource_group.hub_rg.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.hub_vnet.name
   address_prefixes     = ["10.0.0.1/24"]
 }
