fdee27393cfe9ef3395a48795a465c03a8db22d2
learwithpradnya <pradnyadesh10@gmail.com>
2026-02-19 23:16:22 +0530

    updated

--- 13-azure-hub-and-spoke/Hub-network/hub-vnet.tf
+++ 13-azure-hub-and-spoke/Hub-network/hub-vnet.tf
@@ -21,3 +21,41 @@ resource "azurerm_subnet" "firewallsubnet" {
   depends_on = [ azurerm_virtual_network.hub_vnet ]
 }
 
+resource "azurerm_firewall" "firewall" {
+  name                = "hub-firewall"
+  location            = var.location
+  resource_group_name = var.rg_name
+  sku_name = "AZFW_VNet"
+  sku_tier = "Basic"
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.firewallsubnet.id
+    public_ip_address_id = azurerm_public_ip.firewall_public_ip.id
+  }
+
+  depends_on = [azurerm_subnet.firewallsubnet]
+  
+}
+
+resource "azurerm_public_ip" "firewall_public_ip" {
+  name                = "firewall-public-ip"
+  location            = var.location
+  resource_group_name = var.rg_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  
+}
+
+resource "azurerm_route_table" "routetable"{
+  name                = "hub-route-table"
+  location            = var.location
+  resource_group_name = var.rg_name
+  route {
+    name                   = "default-route-to-firewall"
+    address_prefix         = "0.0.0.0/0"
+    next_hop_type          = "VirtualAppliance"
+    next_hop_in_ip_address = azurerm_firewall.firewall.ip_configuration[0].private_ip_address
+    }
+  depends_on = [azurerm_firewall.firewall]
+}
