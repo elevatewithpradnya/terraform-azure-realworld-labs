a3435790bb636920ff731cb8c83a0afef3e7c667
learnwithpradnya <pradnyadesh10@gmail.com>
2026-07-28 21:32:53 +0530

    Updated import.tf

--- 08-brownfield-azure-infra/import.tf
+++ 08-brownfield-azure-infra/import.tf
@@ -1,47 +1,47 @@
 #Import existing resource group and virtual network
 
 import {
-  to = azurerm_resource_group.rg
+  to = module.resource_group.azurerm_resourcegroup.rg
   id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Vnet"
 }
 
 import {
-  to = azurerm_resource_group.keyvaultdemo
+  to = module.resource_group.azurerm_resourcegroup.keyvaultdemo
   id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Keyvault-demo"
 }
 
 import {
 
-  to = azurerm_virtual_network.vnet
+  to = module.network.azurerm_virtual_network.vnet
   id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Vnet/providers/Microsoft.Network/virtualNetworks/vnet-dev"
 }
 
 import {
-  to = azurerm_subnet.subnet
+  to = module.network.azurerm_subnet.subnet1
   id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Vnet/providers/Microsoft.Network/virtualNetworks/vnet-dev/subnets/subnet-dev-01"
 }
 
 import {
-  to = azurerm_subnet.subnet2
+  to = module.network.azurerm_subnet.subnet2
   id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Vnet/providers/Microsoft.Network/virtualNetworks/vnet-dev/subnets/subnet-dev-02"
 }
 
 import {
-  to = azurerm_public_ip.publicip
+  to = module.network.azurerm_public_ip.public_ip
   id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Vnet/providers/Microsoft.Network/publicIPAddresses/vm-linux-01-ip"
 }
 
 import {
-  to = azurerm_network_interface.nic
+  to = module.vm.azurerm_network_interface.nic
   id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Vnet/providers/Microsoft.Network/networkInterfaces/vm-linux-01-nic"
 }
 
 import {
-  to = azurerm_virtual_machine.vm
+  to = module.vm.azurerm_virtual_machine.vm
   id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Vnet/providers/Microsoft.Compute/virtualMachines/vm-linux-01"
 }
 
 import {
-  to = azurerm_key_vault.kv
+  to = module.keyvault.azurerm_key_vault.kv
   id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Keyvault-demo/providers/Microsoft.KeyVault/vaults/kv-dev-app01"
 }
