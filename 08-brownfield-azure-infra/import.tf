d452eab4881e8c361f02ab71a195ac790fc22d0f
learnwithpradnya <pradnyadesh10@gmail.com>
2026-07-28 17:47:51 +0530

    Import.tf updated

--- 08-brownfield-azure-infra/import.tf
+++ 08-brownfield-azure-infra/import.tf
@@ -6,6 +6,11 @@ import {
 }
 
 import {
+  to = azurerm_resource_group.keyvaultdemo
+  id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Keyvault-demo"
+}
+
+import {
 
   to = azurerm_virtual_network.vnet
   id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Vnet/providers/Microsoft.Network/virtualNetworks/vnet-dev"
@@ -18,5 +23,25 @@ import {
 
 import {
   to = azurerm_subnet.subnet2
-  id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Vnet/providers/Microsoft.Network/virtualNetworks/vnet-dev/subnets/suvnet-dev-02"
+  id= "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Vnet/providers/Microsoft.Network/virtualNetworks/vnet-dev/subnets/subnet-dev-02"
+}
+
+import {
+  to = azurerm_public_ip.publicip
+  id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Vnet/providers/Microsoft.Network/publicIPAddresses/vm-linux-01-ip"
+}
+
+import {
+  to = azurerm_network_interface.nic
+  id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Vnet/providers/Microsoft.Network/networkInterfaces/vm-linux-01-nic"
+}
+
+import {
+  to = azurerm_virtual_machine.vm
+  id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Vnet/providers/Microsoft.Compute/virtualMachines/vm-linux-01"
+}
+
+import {
+  to = azurerm_key_vault.kv
+  id = "/subscriptions/a96db7f9-f077-48d6-9d2e-68075185bc77/resourceGroups/Keyvault-demo/providers/Microsoft.KeyVault/vaults/kv-dev-app01"
 }
--- 08-brownfield-azure-infra/main.tf
+++ 08-brownfield-azure-infra/main.tf
@@ -14,33 +14,3 @@ provider "azurerm" {
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 }
-# __generated__ by Terraform
-
-resource "azurerm_resource_group" "rg" {
-  location = "centralindia"
-  name     = "Vnet"
-  tags     = {}
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  address_space       = ["10.0.0.0/16"]
-  location            = "centralindia"
-  name                = "vnet-dev"
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  address_prefixes                = ["10.0.0.0/24"]
-  name                            = "subnet-dev-01"
-  virtual_network_name            = azurerm_virtual_network.vnet.name
-  resource_group_name             = azurerm_resource_group.rg.name
-  default_outbound_access_enabled = false
-}
-
-resource "azurerm_subnet" "subnet2" {
-  address_prefixes                = ["10.0.1.0/28"]
-  name                            = "subnet-dev-02"
-  resource_group_name             = azurerm_resource_group.rg.name
-  virtual_network_name            = azurerm_virtual_network.vnet.name
-  default_outbound_access_enabled = true
-}
